4cdccf4058e008ec4894fb51afe5ed1cabb5fd61
Ryan <rdorrity@gmail.com>
2019-09-05 14:56:18 -0700

    init new project

---
+++ hello-world/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{ message }}</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>   
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return { 
+      message: 'Hello Ryan!' 
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
